18e76279ee1f505b2e0ac27a2a9a95f0b53bc46d
Steven McSorley <steven.mcsorley@eqtr.com>
2022-07-06 16:44:45 +0100

    fixed storybook lint

--- client/gtm-dev/pages/home.stories.jsx
+++
@@ -1,9 +0,0 @@
-import Home from "./index";
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-  title: "Pages/Home",
-  component: Home,
-};
-
-export const HomePage = () => <Home />
